8865eaa002c0e5ecad5f10beec511c03ab020659
Keirafranco <125355714+Keirafranco@users.noreply.github.com>
2023-03-23 09:41:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("keira")
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.clearScreen()
+})
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
